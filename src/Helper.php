b790421f7b8670cd36d159019ba866fe8b7b5b42
sankar <sankar.suda@gmail.com>
2015-06-17 19:19:06 +0530

    added namespace

--- src/Helper.php
+++ src/Helper.php
@@ -8,6 +8,11 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+namespace Speedwork\Core;
+
+/**
+ * @author sankar <sankar.suda@gmail.com>
+ */
 class Helper extends Controller
 {
 }
